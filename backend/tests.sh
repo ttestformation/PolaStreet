d46e34b9389c3b1d2b8fed4d267cd53c7c920d7d
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2023-06-08 16:32:41 +0200

    Test client de visualistion de la map OK

--- backend/tests.sh
+++ backend/tests.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
 # [ "$(curl http://127.0.0.1:8000 | grep 'Map')" ] &&  echo "Test OK"
-[ ! "$(curl http://127.0.0.1:8000 | grep 'Map')" ] &&  echo "Test FAIL"
+[ ! "$(curl http://127.0.0.1:8000 | grep 'Map')" ] &&  echo "Test FAIL" && exit 42
+
+open http://127.0.0.1:8000
