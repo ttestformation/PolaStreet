1e3fa7fa7d3040d9cde7b6bf5398acdedd163b6f
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2023-06-08 16:35:36 +0200

    Test client de visualistion de la map correctif ajustement aux spécifications

--- backend/tests.sh
+++ backend/tests.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # [ "$(curl http://127.0.0.1:8000 | grep 'Map')" ] &&  echo "Test OK"
-[ ! "$(curl http://127.0.0.1:8000 | grep 'Map')" ] &&  echo "Test FAIL" && exit 42
+[ ! "$(curl http://127.0.0.1:8000 | grep 'class="leaflet-pane leaflet-map-pane"')" ] &&  echo "Test FAIL" && exit 42
 
 open http://127.0.0.1:8000
