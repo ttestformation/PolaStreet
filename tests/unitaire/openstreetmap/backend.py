c4631f1ca792dea4c5e0a730ea7e311dff7f3332
Stéphane BILQUÉ <Stephane.Bilque@caissedesdepots.fr>
2023-06-08 14:56:06 +0200

    test(backend): créé un simple HTTP server basé sur HUG
    chore(.gitignore): évite le suivi des modifications du répertoires venv dans tests

--- tests/unitaire/openstreetmap/backend.py
+++ tests/unitaire/openstreetmap/backend.py
@@ -1,20 +1,26 @@
+
+"""Create a simple HTTP server"""
 import hug
 from geopy.geocoders import Nominatim
-adresse = "16 rue berthollet 94110 Arcueil"
 
 geolocator = Nominatim(user_agent="PolaStreet")
 
-def get_location_vers_coordonnees(adresse):
-    '''retourne les coordonnées GPO d'une adresse
-    '''
+api = hug.API(__name__)
+
+@hug.get('/adresse/coordonnees', examples="adresse=16%20rue%20berthollet%2094110%20ARCUEIL")
+@hug.local()
+def get_location_vers_coordonnees(adresse: hug.types.text):
+    '''Retourne les coordonnées GPS d'une adresse
+    :param string adresse: L'adresse à géocoder.
+    :return: les coordonnées GPS'''
     location = geolocator.geocode(adresse)
     if location is not None:
         latitude = location.latitude
         longitude = location.longitude
         return {'latitude': latitude, 'longitude': longitude}
     else:
-        return {'erreur': 'adresse non trouvé'}
+        return {'erreur': 'adresse non trouvée'}
 
-
-coordonnees = get_location_vers_coordonnees(adresse)
-
+if __name__ == '__main__':
+    api = hug.API(__name__)
+    api.http.serve(port=8080)
