98fa729aa9aafc0f12d0afe304f5bdfe393941c5
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2023-06-09 09:53:13 +0200

    DDD - business path for Map interactions

--- backend/pages/urls.py
+++ backend/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, AddressePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
+    path("adress/", AddressePageView.as_view(), name="address"),
 ]
