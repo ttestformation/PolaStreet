8c518b1e146d09609212ee5aac16c5184c9e506a
Stéphane BILQUÉ <Stephane.Bilque@caissedesdepots.fr>
2023-06-09 09:55:55 +0200

    tests(simple_server.py): permet d'utiliser la variable d'environnement HOST_ADDRESS lors des tests pytest

--- tests/unitaire/pytests/test_simple_server.py
+++ tests/unitaire/pytests/test_simple_server.py
@@ -1,24 +1,37 @@
 import requests
 import pytest
 import urllib
+import os
+import json
 
-def ping_site(host='127.0.0.1', port=8080):
-    uri = 'http://{}:{}'.format(host,port)
-    result = requests.get(uri).json()
-    overview = result.get('documentation',{}).get('overview',None)
-    if overview:
-        return overview
-    else:
-        raise "500"
+def validateJSON(jsonData):
+    try:
+        json.loads(jsonData)
+    except ValueError as err:
+        return False
+    return True
 
-def get_adresse_coordonnees(host='127.0.0.1', port=8080, adresse=''):
+def ping_site(url=os.environ.get('HOST_ADDRESS', 'http://127.0.0.1:8080')):
+    result = requests.get(url)
+    overview = "500"
+    if result.status_code==404:
+        if validateJSON(result.text):
+            result_json = result.json()
+            overview = result_json .get('documentation',{}).get('overview',None)
+    return overview
+
+def get_adresse_coordonnees(url=os.environ.get('HOST_ADDRESS', 'http://127.0.0.1:8080'), adresse=''):
     safe_adresse = urllib.request.pathname2url(adresse)
-    uri = 'http://{}:{}/adresse/coordonnees?adresse={}'.format(host,port,safe_adresse)
-    result = requests.get(uri).json()
-    latitude = result.get('latitude',None)
-    longitude = result.get('longitude',None)
+    uri = '{}/adresse/coordonnees?adresse={}'.format(url,safe_adresse)
+    latitude,longitude = None,None
+    result = requests.get(uri)
+    if result:
+        if validateJSON(result.text):
+            result_json  = result.json()
+            latitude = result_json .get('latitude',None)
+            longitude = result_json .get('longitude',None)
     return latitude,longitude
-
+    
 def test_ping_site():
     assert ping_site() == "Create a simple HTTP server"
     
